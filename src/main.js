5d1d8da0043c282bdd644eea0b887191950e2f18
mangmuse <mangmangmuse@gmail.com>
2024-02-06 16:44:49 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -28,3 +28,53 @@ function displayArrow() {
     arrow.classList.add("hidden");
   }
 }
+
+// 1. 모바일 버전의 경우 nav가 표시되지않고 햄버거가 나옴
+// 2. 햄버거를 누르면 세로로 nav가 표시됨
+const toggleBtn = document.querySelector(".header__toggle");
+const headerMenu = document.querySelector(".header__menu");
+toggleBtn.addEventListener("click", toggleNav);
+function toggleNav(e) {
+  headerMenu.classList.toggle("open");
+}
+// 3. nav 버튼을 누르면 해당 페이지로 이동이 되고 nav 창이 닫힘
+// const homeSection = document.querySelector("#home");
+// const aboutSection = document.querySelector("#about");
+// const skillsSection = document.querySelector("#skills");
+// const workSection = document.querySelector("#work");
+// const testimonialSection = document.querySelector("#testimonial");
+// const contactSection = document.querySelector("#contact");
+const main = document.querySelector("main");
+console.log(main.childNodes);
+
+headerMenu.addEventListener("click", jump);
+function jump(e) {
+  switch (e.target.innerText) {
+    case "Home":
+      const homeSection = main.childNodes[3];
+      homeSection.scrollIntoView();
+      break;
+    case "About":
+      const aboutSection = main.childNodes[7];
+      aboutSection.scrollIntoView();
+      break;
+    case "Skills":
+      const skillSection = main.childNodes[11];
+      skillSection.scrollIntoView();
+
+      break;
+    case "My work":
+      const workSection = main.childNodes[15];
+      workSection.scrollIntoView();
+      break;
+    case "Testimonials":
+      const testimonialSection = main.childNodes[19];
+      testimonialSection.scrollIntoView();
+      break;
+    case "Contact":
+      const contactSection = document.querySelector("#contact");
+      contactSection.scrollIntoView();
+      break;
+  }
+  toggleNav();
+}
