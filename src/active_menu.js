ac72ec90519388d17b7f06cfd83da8e9c3360878
mangmuse <mangmangmuse@gmail.com>
2024-02-08 04:59:53 +0900

    Observer-part1

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonial",
+  "#contact",
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
+
+const visibleSections = sectionIds.map(() => false);
+console.log("dd", visibleSections);
+
+const options = {};
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+  let selectLastOne;
+  entries.forEach((entry) => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    console.log(index);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.99;
+    console.log(visibleSections);
+    console.log("무라섹", selectLastOne);
+  });
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+  console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
